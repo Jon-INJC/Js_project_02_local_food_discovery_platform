d76e5dd6e0aa2867e8c63dc931db2293915657c1
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-20 14:26:20 +0300

    Finished the esplore page

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -82,27 +82,124 @@ function Explore() {
             <h2 className="max-w-md text-left text-4xl font-bold font-main-header">
               Curated Collections
             </h2>
-            <div className="flex gap-6 pl-6 overflow-x-auto">
-                <div className="flex flex-col items-start shrink-0">
-                    <img src="https://placehold.co/300x400/orange/white" alt="" className="w-50 h-55 object-cover" />
-                    <h3 className="text-left text-2xl font-bold font-main-header">Morning Brews</h3>
-                    <p className="text-sm text-secondary">12 Spots</p>
+            <div className="flex gap-6 pl-6 overflow-x-auto no-scrollbar">
+              <div className="flex flex-col items-start shrink-0">
+                <img
+                  src="https://placehold.co/300x400/orange/white"
+                  alt=""
+                  className="w-50 h-55 object-cover"
+                />
+                <h3 className="text-left text-2xl font-bold font-main-header">
+                  Morning Brews
+                </h3>
+                <p className="text-sm text-secondary">12 Spots</p>
+              </div>
+              <div className="flex flex-col items-start shrink-0">
+                <img
+                  src="https://placehold.co/300x400/orange/white"
+                  alt=""
+                  className="w-50 h-55 object-cover"
+                />
+                <h3 className="text-left text-2xl font-bold font-main-header">
+                  Artisan Pastry
+                </h3>
+                <p className="text-sm text-secondary">8 Spots</p>
+              </div>
+              <div className="flex flex-col items-start shrink-0">
+                <img
+                  src="https://placehold.co/300x400/orange/white"
+                  alt=""
+                  className="w-50 h-55 object-cover"
+                />
+                <h3 className="text-left text-2xl font-bold font-main-header">
+                  Haute Cuisine
+                </h3>
+                <p className="text-sm text-secondary">4 Spots</p>
+              </div>
+              <div className="flex flex-col items-start shrink-0">
+                <img
+                  src="https://placehold.co/300x400/orange/white"
+                  alt=""
+                  className="w-50 h-55 object-cover"
+                />
+                <h3 className="text-left text-2xl font-bold font-main-header">
+                  Evening Spirits
+                </h3>
+                <p className="text-sm text-secondary">6 Spots</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="w-screen max-w-4xl px-6 mx-auto">
+          <div className="container flex flex-col  space-y-6 mt-10">
+            <div className="flex items-center justify-between border-b-2 border-solid border-outline-variant">
+              <h2 className="max-w-md text-left text-4xl font-bold font-main-header">
+                The Editorial Selection
+              </h2>
+            </div>
+            <div className="flex flex-col items-center space-y-4 md:justify-end md:items-start md:flex-row md:space-x-6">
+              <div className="flex flex-col items-start">
+                <img
+                  src="https://placehold.co/300x400/orange/white"
+                  alt=""
+                  className="max-w-75 max-h-100 object-cover"
+                />
+                <div className="flex flex-col gap-2 items-start">
+                  <p className="text-xs text-secondary">
+                    CONTEMPORARY JAPANESE . $$$$
+                  </p>
+                  <h3 className="text-left text-4xl font-bold font-main-header">
+                    Omaske Lumière
+                  </h3>
+                  <p className="text-xs text-secondary max-w-75 block">
+                    An immersive dining experience redefining traditional edomae
+                    sushi with modern European techniques. The tasting menu
+                    changes daily based on Tsukiji market arrivals.
+                  </p>
+                  <div className=" container flex gap-4 items-center pt-6 border-t-2 border-solid border-outline-variant">
+                    <img
+                      src="https://placehold.co/100x100/orange/white"
+                      alt=""
+                      className="w-15 h-15 object-cover rounded-md"
+                    />
+                    <div className="flex flex-col text-xs text-secondary">
+                      <p>MUST TRY</p>
+                      <p className="text-on-surface">Smoked Toro with Truffle Caviar</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-col items-start shrink-0">
-                    <img src="https://placehold.co/300x400/orange/white" alt="" className="w-50 h-55 object-cover" />
-                    <h3 className="text-left text-2xl font-bold font-main-header">Artisan Pastry</h3>
-                    <p className="text-sm text-secondary">8 Spots</p>
+              </div>
+
+              <div className="flex flex-col items-start">
+                <img
+                  src="https://placehold.co/300x400/orange/white"
+                  alt=""
+                  className="max-w-75 max-h-100 object-cover"
+                />
+                <div className="flex flex-col gap-2 items-start">
+                  <p className="text-xs text-secondary">
+                    RUSTIC MEDITERRANEAN . $$$$
+                  </p>
+                  <h3 className="text-left text-4xl font-bold font-main-header">
+                    Osteria Delle Vigne
+                  </h3>
+                  <p className="text-xs text-secondary max-w-75 block">
+                    Hidden away in a lush courtyard, serving regional Italian
+                    classics with a focus on ancient grains and natural wines.
+                  </p>
+                  <div className="container flex gap-4 items-center pt-6 border-t-2 border-solid border-outline-variant">
+                    <img
+                      src="https://placehold.co/100x100/orange/white"
+                      alt=""
+                      className="w-15 h-15 object-cover rounded-md"
+                    />
+                    <div className="flex flex-col text-xs text-secondary">
+                      <p>SIGNATURE</p>
+                      <p className="text-on-surface">Hand-rolled Pici Cacio e Pepe</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-col items-start shrink-0">
-                    <img src="https://placehold.co/300x400/orange/white" alt="" className="w-50 h-55 object-cover" />
-                    <h3 className="text-left text-2xl font-bold font-main-header">Haute Cuisine</h3>
-                    <p className="text-sm text-secondary">4 Spots</p>
-                </div>
-                <div className="flex flex-col items-start shrink-0">
-                    <img src="https://placehold.co/300x400/orange/white" alt="" className="w-50 h-55 object-cover" />
-                    <h3 className="text-left text-2xl font-bold font-main-header">Evening Spirits</h3>
-                    <p className="text-sm text-secondary">6 Spots</p>
-                </div>
+              </div>
             </div>
           </div>
         </section>
