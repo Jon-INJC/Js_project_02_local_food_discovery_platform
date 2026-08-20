5a46463084907967d64dad435b838ce5b5d32ec1
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-20 11:04:08 +0300

    Created the explore page

---
+++ src/pages/explore.jsx
@@ -0,0 +1,86 @@
+import Nav from "../components/navBar";
+import Footer from "../components/footer";
+
+function Explore() {
+  return (
+    <>
+      <Nav />
+      <main className="bg-on-tertiary">
+        <section id="discover" className="w-screen max-w-4xl px-6 mx-auto">
+          <div className="container flex flex-col">
+            <div className="hidden md:flex md:flex-col md:space-y-2">
+              <h1 className="font-bold font-main-header text-5xl text-left text-on-surface">
+                Discover Extraordinary Culinary Experiences
+              </h1>
+              <p className="max-w-md text-center text-secondary md:text-left">
+                Curated selections tailored to your palate.
+              </p>
+            </div>
+
+            <div className="flex justify-between md:hidden">
+              <h1 className="max-w-md font-bold font-main-header text-5xl text-left text-on-surface">
+                Explore
+              </h1>
+              <select
+                name="location"
+                id="location"
+                className="border-none outline-none focus:ring-0"
+              >
+                <option value="">Current location</option>
+              </select>
+            </div>
+            <div className="container flex items-center justify-between mt-4 ">
+              <input
+                className="w-100 underline underline-offset-3 border-none outline-none focus:ring-0"
+                type="text"
+                placeholder="Search cuisines, restaurants, or dishes..."
+              />
+              <select
+                name="location"
+                id="location"
+                className="hidden underline underline-offset-4 border-none outline-none focus:ring-0 text-secondary md:inline-block"
+              >
+                <option value="">Current location</option>
+              </select>
+            </div>
+            <div className="container flex space-x-6 mt-4 text-xs font-bold border-b border-outline-variant">
+              <a
+                href="#"
+                className="text-secondary hover:text-primary hover:underline hover:-mt-1 decoration-primary"
+              >
+                COFFEE
+              </a>
+              <a
+                href="#"
+                className="text-secondary hover:text-primary hover:underline hover:-mt-1 decoration-primary"
+              >
+                FINE DINING
+              </a>
+              <a
+                href="#"
+                className="text-secondary hover:text-primary hover:underline hover:-mt-1 decoration-primary"
+              >
+                BREAKFAST
+              </a>
+              <a
+                href="#"
+                className="text-secondary hover:text-primary hover:underline hover:-mt-1 decoration-primary"
+              >
+                SUSHI
+              </a>
+              <a
+                href="#"
+                className="text-secondary hover:text-primary hover:underline hover:-mt-1 decoration-primary"
+              >
+                VEGAN
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Explore;
