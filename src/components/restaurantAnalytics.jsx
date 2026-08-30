20e6c6a2419cfd76bac58cee5f53e57621e9a894
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-30 11:53:29 +0300

    Added the analytics section of the dash

---
+++ src/components/restaurantAnalytics.jsx
@@ -0,0 +1,118 @@
+import { Eye } from "lucide-react";
+import navBarLogo from "../assets/Aura_logo.svg";
+function DashAnalytics() {
+  return (
+    <>
+      <div className="flex items-center justify-between md:hidden">
+        <div className="flex items-center">
+          <img src={navBarLogo} alt="" />
+        </div>
+        <select
+          name="date"
+          id="date"
+          className="text-xs text-secondary px-3 py-2 border-2 border-secondary rounded-sm hover:cursor-pointer"
+        >
+          <option value="">Last 30 Days</option>
+        </select>
+      </div>
+      <div className="container mt-7 flex flex-col gap-y-4 md:mt-0">
+        <div className="flex items-start justify-between pb-5 border-b-2 border-outline-variant md:items-end">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-4xl text-on-surface font-bold font-main-header">
+              DashAnalytics
+            </h1>
+            <p className="text-xs text-secondary md:text-sm md:max-w-full">
+              Understand how guests discover and interact with your restaurant
+              and menu.
+            </p>
+          </div>
+          <select
+            name="date"
+            id="date"
+            className="hidden text-xs text-secondary px-3 py-2 border-2 border-secondary rounded-sm hover:cursor-pointer md:block"
+          >
+            <option value="">Last 30 Days</option>
+          </select>
+        </div>
+      </div>
+        <div className="container mt-7 grid grid-cols-12 gap-4">
+          <div className="min-h-35 p-3 flex flex-col justify-between col-span-6 rounded-md border-2 border-outline-variant md:col-span-3">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-on-surface-variant font-medium">
+                Menu View
+              </p>
+              <Eye className="w-5 h-5 text-primary cursor-pointer" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <span className="text-4xl text-on-surface font-bold font-main-header">
+                12.4K
+              </span>
+              <span className="text-xs text-primary font-medium">+14%</span>
+            </div>
+          </div>
+          {/* second card */}
+          <div className="min-h-35 p-3 flex flex-col justify-between col-span-6 rounded-md border-2 border-outline-variant md:col-span-3">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-on-surface-variant font-medium">
+                Menu View
+              </p>
+              <Eye className="w-5 h-5 text-primary cursor-pointer" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <span className="text-4xl text-on-surface font-bold font-main-header">
+                12.4K
+              </span>
+              <span className="text-xs text-primary font-medium">+14%</span>
+            </div>
+          </div>
+          <div className="min-h-35 p-3 flex flex-col justify-between col-span-6 rounded-md border-2 border-outline-variant md:col-span-3">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-on-surface-variant font-medium">
+                Menu View
+              </p>
+              <Eye className="w-5 h-5 text-primary cursor-pointer" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <span className="text-4xl text-on-surface font-bold font-main-header">
+                12.4K
+              </span>
+              <span className="text-xs text-primary font-medium">+14%</span>
+            </div>
+          </div>
+          <div className="min-h-35 p-3 flex flex-col justify-between col-span-6 rounded-md border-2 border-outline-variant md:col-span-3">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-on-surface-variant font-medium">
+                Menu View
+              </p>
+              <Eye className="w-5 h-5 text-primary cursor-pointer" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <span className="text-4xl text-on-surface font-bold font-main-header">
+                12.4K
+              </span>
+              <span className="text-xs text-primary font-medium">+14%</span>
+            </div>
+          </div>
+        </div>
+        <div className="container mt-7 p-6 border-2 border-outline-variant">
+            <div className="flex items-center justify-between border-b-2 border-outline-variant">
+                <span className="text-xl text-on-surface font-bold font-main-header">Views & Engagement Over Time</span>
+                <div className="flex gap-x-3 items-center">
+                    <a href="#" className="text-xs pb-1 text-secondary font-semibold hover:text-primary hover:border-b-2 hover:border-primary hover:-translate-y-1">All Metrics</a>
+                </div>
+            </div>
+            <div className="container"></div>
+        </div>
+        <div className="container mt-7 grid gap-4 grid-cols-12 grid-rows-12">
+            <div className="col-span-12 row-span-12 p-4 border-2 border-outline-variant md:col-span-7">
+                <span className="block pb-2 mb-4 text-xl text-on-surface font-bold font-main-header border-b-2 border-outline-variant">Most Engaged Menu Items</span>
+                <div className="flex items-center gap-x-2"></div>
+            </div>
+            <div className="hidden col-span-5 row-span-8"></div>
+            <div className="hidden col-span-5 row-span-4"></div>
+        </div>
+    </>
+  );
+}
+
+export default DashAnalytics;
--- src/pages/restaurantDash.jsx
+++ src/pages/restaurantDash.jsx
@@ -1,25 +1,33 @@
 import navBarLogo from "../assets/Aura_logo.svg";
-import { LayoutDashboard, UtensilsCrossed } from "lucide-react";
+import { LayoutDashboard, UtensilsCrossed, ChartNoAxesCombined, MessageSquareDiff } from "lucide-react";
 //import Overview from "../components/restaurantOverview";
-import DashMenu from "../components/restaurantMenu"
+//import DashMenu from "../components/restaurantMenu";
+import DashAnalytics from "../components/restaurantAnalytics";
 function RestaurantDash() {
   return (
     <>
       <main className="bg-surface-bright md:flex md:flex-row-reverse">
         <section className="p-6 md:p-20 md:grow">
             {/* <Overview /> */}
-            <DashMenu />
+            {/* <DashMenu /> */}
+            <DashAnalytics />
         </section>
         <aside className="hidden w-65 p-2 border-2 border-outline-variant md:min-h-screen md:flex md:flex-col md:gap-y-6">
           <div className="flex items-center p-4">
             <img src={navBarLogo} alt="" />
           </div>
-          <div className="flex flex-col gap-y-4 p-4">
-            <a href="#" className="text-secondary text-sm flex gap-x-2">
+          <div className="flex flex-col gap-y-6 p-4">
+            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
               <LayoutDashboard className="w-5 h-5" /> Overview
             </a>
-            <a href="#" className="text-secondary text-sm flex gap-x-2">
+            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
               <UtensilsCrossed className="w-5 h-5" /> Menu
+            </a>
+            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
+              <ChartNoAxesCombined className="w-5 h-5" /> Analytics
+            </a>
+            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
+              <MessageSquareDiff className="w-5 h-5" /> Review
             </a>
           </div>
         </aside>
