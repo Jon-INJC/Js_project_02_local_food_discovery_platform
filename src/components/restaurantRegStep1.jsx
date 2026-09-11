7a68815d6f221ffa228d822198608a0a679c8854
Jon-INJC <chalachewyonathan8@gmail.com>
2026-09-11 11:36:21 +0300

    Added the form handling and validation for the restaurant registration form step 1

--- src/components/restaurantRegStep1.jsx
+++ src/components/restaurantRegStep1.jsx
@@ -1,12 +1,21 @@
+import { useForm } from "react-hook-form";
+import Error from "./error";
 function RegStep1() {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const onSubmit = (data) => {
+    console.log("Form is Submitted", data);
+  };
+
   return (
     <>
       <div className="container max-w-125 mx-auto pt-10 flex flex-col gap-y-4 items-center">
         <div className="flex flex-col items-center gap-y-2">
-            <p className="text-sm text-secondary font-bold">STEP 1 OF 3</p>
-            <div className="w-50 h-1 bg-surface-variant rounded-full">
-                <div className="w-1/3 h-1 bg-primary rounded-full"></div>
-            </div>
+          <p className="text-sm text-secondary font-bold">STEP 1 OF 3</p>
+          <div className="w-50 h-1 bg-surface-variant rounded-full">
+            <div className="w-1/3 h-1 bg-primary rounded-full"></div>
+          </div>
         </div>
         <div className="flex flex-col gap-y-4 items-centers">
           <h1 className="text-4xl text-center text-on-surface font-bold font-main-header">
@@ -17,8 +26,12 @@ function RegStep1() {
             with the essentials.
           </p>
         </div>
-        <form className="w-[90%] flex flex-col p-6 gap-y-5 bg-on-tertiary border-2 border-outline-variant">
-          <div className="flex flex-col gap-y-2 border-b-2 border-outline-variant">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          className="w-[90%] flex flex-col p-6 gap-y-5 bg-on-tertiary border-2 border-outline-variant"
+        >
+          <div className="flex flex-col gap-y-2">
             <label
               htmlFor="restaurantName"
               className="text-sm text-on-surface-variant font-bold"
@@ -30,34 +43,64 @@ function RegStep1() {
               id="restaurantName"
               placeholder="The French Laundry"
               required
-              className="border-none outline-none focus:ring-0"
+              {...register("restaurantName", {
+                required: {
+                  value: true,
+                  message: "Restaurant Name is Required",
+                },
+              })}
+              className="border-b-2 border-outline-variant outline-none focus:ring-0"
             />
+            <Error text={errors.restaurantName?.message} />
           </div>
-          <div className="flex flex-col gap-y-2 border-b-2 border-outline-variant">
+          <div className="flex flex-col gap-y-2">
             <label
               htmlFor="Category"
               className="text-sm text-on-surface-variant font-bold"
             >
               Cuisine / Category
             </label>
-            <select id="Category" className="border-none outline-none focus:ring-0">
-                <option value="">Select primary cuisine</option>
-            </select>
+            <input
+              list="items-list"
+              id="category"
+              placeholder="Select or type..."
+              required
+              {...register("category", {
+                required: {
+                  value: true,
+                  message: "Restaurant category is Required",
+                },
+              })}
+              className="border-b-2 border-outline-variant outline-none focus:ring-0"
+            />
+            <Error text={errors.category?.message} />
+            <datalist id="items-list">
+              <option value="Apple"></option>
+              <option value="Banana"></option>
+              <option value="Cherry"></option>
+            </datalist>
           </div>
-          <div className="flex flex-col gap-y-2 border-b-2 border-outline-variant">
+          <div className="flex flex-col gap-y-2">
             <label
-              htmlFor="Location"
+              htmlFor="location"
               className="text-sm text-on-surface-variant font-bold"
             >
               Location
             </label>
             <input
               type="text"
-              id="Location"
+              id="location"
               placeholder="Full street address"
               required
-              className="border-none outline-none focus:ring-0"
+              {...register("location", {
+                required: {
+                  value: true,
+                  message: "Location is Required",
+                },
+              })}
+              className="border-b-2 border-outline-variant outline-none focus:ring-0"
             />
+            <Error text={errors.location?.message} />
           </div>
           <div className="flex flex-col gap-y-2">
             <label
@@ -66,11 +109,21 @@ function RegStep1() {
             >
               About the Restaurant
             </label>
-            <textarea name="discription" id="discription" maxLength={300} rows={3} className="border-2 border-outline-variant outline-none focus:ring-0"></textarea>
-            <p className="text-xs text-secondary self-end">0 / 300 characters</p>
+            <textarea
+              name="discription"
+              id="discription"
+              maxLength={300}
+              rows={3}
+              className="border-2 border-outline-variant outline-none focus:ring-0"
+            ></textarea>
+            <p className="text-xs text-secondary self-end">
+              0 / 300 characters
+            </p>
           </div>
           <div className="py-6 flex items-end justify-end">
-            <button className="px-4 py-2 bg-secondary text-on-tertiary cursor-pointer">Continue <i class="fa-solid fa-arrow-right"></i></button>
+            <button className="px-4 py-2 bg-secondary text-on-tertiary cursor-pointer">
+              Continue <i class="fa-solid fa-arrow-right"></i>
+            </button>
           </div>
         </form>
       </div>
