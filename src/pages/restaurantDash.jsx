7dbb364ce1035cc13f7f777bcae2b26af35c518a
Jon-INJC <chalachewyonathan8@gmail.com>
2026-09-13 11:46:13 +0300

    Added the navigation and the click effect for the dashboard components

--- src/pages/restaurantDash.jsx
+++ src/pages/restaurantDash.jsx
@@ -1,34 +1,55 @@
 import navBarLogo from "../assets/Aura_logo.svg";
-import { LayoutDashboard, UtensilsCrossed, ChartNoAxesCombined, MessageSquareDiff } from "lucide-react";
-import { Outlet } from "react-router-dom";
+import {
+  LayoutDashboard,
+  UtensilsCrossed,
+  ChartNoAxesCombined,
+  MessageSquareDiff,
+} from "lucide-react";
+import { NavLink, Outlet } from "react-router-dom";
+
+const NAV_ITEMS = [
+{ title: "Overview", path: "/dashboard/overview", icon: LayoutDashboard },
+{ title: "Menu", path: "/dashboard/menu", icon: UtensilsCrossed },
+{ title: "Analytics", path: "/dashboard/analytics", icon: ChartNoAxesCombined },
+{ title: "Review", path: "/dashboard/review", icon: MessageSquareDiff },
+];
+
 function RestaurantDash() {
   return (
     <>
       <main className="bg-surface-bright md:flex md:flex-row-reverse">
         <section className="min-w-0 flex-1 p-6 md:p-20">
-            <Outlet />
+          <Outlet />
         </section>
         <aside className="hidden w-65 p-2 border-2 border-outline-variant md:min-h-screen md:flex md:flex-col md:gap-y-6">
           <div className="flex items-center p-4">
             <img src={navBarLogo} alt="" />
           </div>
-          <div className="flex flex-col gap-y-6 p-4">
-            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
-              <LayoutDashboard className="w-5 h-5" /> Overview
-            </a>
-            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
-              <UtensilsCrossed className="w-5 h-5" /> Menu
-            </a>
-            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
-              <ChartNoAxesCombined className="w-5 h-5" /> Analytics
-            </a>
-            <a href="#" className="text-secondary text-sm font-medium flex gap-x-2">
-              <MessageSquareDiff className="w-5 h-5" /> Review
-            </a>
-          </div>
+          <nav className="flex flex-col gap-y-6 p-4">
+            {NAV_ITEMS.map((item) => (
+            <NavItem key={item.path} {...item} />
+          ))}
+          </nav>
         </aside>
       </main>
     </>
+  );
+}
+
+function NavItem({ title, path, icon: Icon }) {
+
+  return (
+    <NavLink
+      to={path}
+      className={({ isActive }) =>
+        `font-medium flex items-center gap-x-2 transition-colors ${
+          isActive ? "text-primary text-base translate-x-1" : "text-secondary text-sm"
+        }`
+      }
+    >
+      {Icon && <Icon className="w-5 h-5" />}{" "}
+      {title}
+    </NavLink>
   );
 }
 
